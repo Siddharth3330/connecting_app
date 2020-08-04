77fc1bdb4f80131b157d79eec5d3d37d2e624cfd
Siddharth Singh <siddharthsingh58000@gmail.com>
2020-08-04 20:48:55 +0530

    start page1.1

---
+++ lib/pages/HomePage.dart
@@ -0,0 +1,38 @@
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+class HomePage extends StatefulWidget {
+  @override
+  _HomePageState createState() => _HomePageState();
+}
+
+
+
+class _HomePageState extends State<HomePage> {
+
+  bool isSignedIn= false;
+
+  Widget buildHomeScreen(){
+    return Text('already singed in');
+  }
+
+  //fuction to call our Signin or Signup activity
+  Scaffold buildSignInScreen(){
+         return Scaffold(
+           body: Text('SignIn Screen', style: TextStyle(fontSize: 40),),
+         );
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    if (isSignedIn){
+
+      return buildHomeScreen();
+    }
+    else{
+
+      return buildSignInScreen();
+    }
+  }
+}
+
---
+++ lib/pages/StartPage.dart
@@ -0,0 +1,56 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+
+import 'HomePage.dart';
+
+class Start extends StatefulWidget {
+  //4 second Delay code start
+  @override
+  _StartState createState() => _StartState();
+}
+
+class _StartState extends State<Start> {
+  @override
+  void initState() {
+    // TODO: implement initState
+    super.initState();
+    startTimer();
+  }
+
+  startTimer() async {
+    var duration= Duration(seconds: 4);
+    return Timer(duration, route);
+  }
+
+  route(){
+    Navigator.pushReplacement( context, MaterialPageRoute(
+        builder: (context) => HomePage(),
+    ));
+  }
+// 4 second delay code ends with calling HomePage widget
+  @override
+  Widget build(BuildContext context) {
+    return
+      Scaffold(
+          body: Container(
+            alignment: Alignment.center,
+            decoration: BoxDecoration(gradient: LinearGradient(
+              begin: Alignment.topRight,
+              end: Alignment.bottomLeft,
+              colors: [Theme.of(context).accentColor, Theme.of(context).primaryColor ],
+            )),
+            child:  Column(
+              mainAxisAlignment: MainAxisAlignment.center,
+              crossAxisAlignment: CrossAxisAlignment.center,
+              children: <Widget>[
+                Text("Connect",
+                  style: TextStyle(
+                    fontSize: 92, color: Colors.black, fontFamily: 'Signatra',
+                  ),)
+              ],),
+          )
+      );
+  }
+}
